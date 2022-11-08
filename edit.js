b6fac45f2e7c7ac72d9b624d99f028b32a9a490f
NFTOON <NFTOON@HOTMAIL.COM>
2022-11-07 21:17:15 -0800

    lfg

---
+++ edit.js
@@ -0,0 +1,28 @@
+import { Configuration, OpenAIApi } from "openai";
+import { createReadStream, writeFileSync } from "fs";
+
+const configuration = new Configuration ({
+    apiKey : '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',
+});
+
+const openai = new OpenAIApi (configuration);
+
+const src = './1667883955908.png'
+const mask = './mask.png'
+
+    const result = await openai.createImageEdit(
+            createReadStream(src),
+            createReadStream(mask),
+            'a programmer drawing AI art',
+            1,
+            '1024x1024',
+    )
+
+const url = result.data.data[0].url;
+console
+
+//Save Image URL to disk
+const imgResult = await fetch(url);
+const blob = await imgResult.blob();
+const buffer = Buffer.from(await blob.arrayBuffer())
+writeFileSync(`./img/${Date.now()}.png`, buffer);
---
+++ variation.js
@@ -0,0 +1,27 @@
+import { Configuration, OpenAIApi } from "openai";
+import { createReadStream, writeFileSync } from "fs";
+
+const configuration = new Configuration ({
+    apiKey : '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',
+});
+
+const openai = new OpenAIApi (configuration);
+
+const prompt = 'a 3d render of protesters in cyberpunk times square'
+
+const src = './1667882837895.png'
+
+const result = await openai.createImageVariation(
+    createReadStream(`./img/${src}`),
+    1,
+    "1024x1024",
+);
+
+const url = result.data.data[0].url;
+console
+
+//Save Image URL to disk
+const imgResult = await fetch(url);
+const blob = await imgResult.blob();
+const buffer = Buffer.from(await blob.arrayBuffer())
+writeFileSync(`./img/${Date.now()}.png`, buffer);
